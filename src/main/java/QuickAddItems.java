992fb79833d295f2481de88ae11469c5dbc3b566
David Santos <dsdsantos973@gmail.com>
2022-06-11 15:22:02 -0400

    created a QuickAddItems class to enter new items

---
+++ src/main/java/QuickAddItems.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class QuickAddItems {
+
+    protected Scanner input = new Scanner(System.in);
+    private String item;
+
+
+    public QuickAddItems(){
+        System.out.println("Enter the items here:");
+        item = input.next();
+
+
+    } // end of constructor
+
+} // end of class
