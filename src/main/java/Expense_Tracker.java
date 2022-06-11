2f8da040735748e14296f64af7dfa7c85cfc67f9
David Santos <dsdsantos973@gmail.com>
2022-06-11 16:28:39 -0400

    adding JBDC into the project

--- src/main/java/Expense_Tracker.java
+++ src/main/java/Expense_Tracker.java
@@ -5,6 +5,11 @@ public class Expense_Tracker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
+
+
+
+
         System.out.println("Hello there! Would you like to start recording your expenses? Y/N");
         String response = input.next();
 
