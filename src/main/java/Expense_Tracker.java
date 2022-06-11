5171cb0aec4c86304db2be64136762595810086a
David Santos <dsdsantos973@gmail.com>
2022-06-11 05:28:57 -0400

    created the main method of the class

--- src/main/java/Expense_Tracker.java
+++ src/main/java/Expense_Tracker.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Expense_Tracker {
-}
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello there! Would you like to start recording your expenses? Y/N");
+        String response = input.next();
+
+        if(response.equals("Yes")){
+
+        } else {
+
+            System.out.println("Goodbye");
+        }
+
+    } // end of main method
+
+} // end of class
